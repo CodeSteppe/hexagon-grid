31ac544c13c070c7c7773ece6bd459537604e2d2
MudOnTire <895157882@qq.com>
2022-05-04 12:42:39 +0800

    update

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-const rowHeight = 110 - 32;
-const colWidth = 100;
+const sideLength = 100;
+const hexagonWidth = Math.sqrt(3) * sideLength;
+const hexagonHeight = 2 * sideLength;
+const gap = 0.02 * hexagonWidth;
+const colWidth = hexagonWidth + gap;
+const rowHeight = 1.5 * sideLength + Math.sqrt(0.75) * gap;
+
+const root = document.documentElement;
+
+root.style.setProperty('--width', hexagonWidth + 'px');
+root.style.setProperty('--height', hexagonHeight + 'px');
+root.style.setProperty('--gap', gap + 'px');
 
 function createGrid() {
-  const grid = document.querySelector('.hexagon-grid');
+  const grid = document.querySelector('.grid');
   const gridWidth = grid.clientWidth;
   const gridHeight = grid.clientHeight;
-  const rowCount = Math.ceil(gridHeight / rowHeight);
-  const colCount = Math.ceil(gridWidth / colWidth);
+  let rowCount = Math.ceil(gridHeight / rowHeight) + 1;
+  let colCount = Math.ceil(gridWidth / colWidth) + 1;
 
   const rows = [];
   for (let i = 0; i < rowCount; i++) {
