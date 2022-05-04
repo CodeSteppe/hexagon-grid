c69d79d91428249dbdf0d24cefd05ba58e8f1923
MudOnTire <895157882@qq.com>
2022-05-04 17:19:36 +0800

    update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// 六边形的边长
 const sideLength = 100;
 const hexagonWidth = Math.sqrt(3) * sideLength;
 const hexagonHeight = 2 * sideLength;
 const gap = 0.02 * hexagonWidth;
+// 每一列的宽度
 const colWidth = hexagonWidth + gap;
+// 每一行的高度，为六边形的高度减去重叠部分的高度，加上行间距的高度
 const rowHeight = 1.5 * sideLength + Math.sqrt(0.75) * gap;
 
 const root = document.documentElement;
@@ -15,8 +18,8 @@ function createGrid() {
   const grid = document.querySelector('.grid');
   const gridWidth = grid.clientWidth;
   const gridHeight = grid.clientHeight;
-  let rowCount = Math.ceil(gridHeight / rowHeight) + 1;
-  let colCount = Math.ceil(gridWidth / colWidth) + 1;
+  const rowCount = Math.ceil(gridHeight / rowHeight) + 1;
+  const colCount = Math.ceil(gridWidth / colWidth) + 1;
 
   const rows = [];
   for (let i = 0; i < rowCount; i++) {
@@ -34,6 +37,4 @@ function createGrid() {
 
 createGrid();
 
-window.addEventListener('resize', e => {
-  createGrid();
-});
+window.addEventListener('resize', createGrid);
